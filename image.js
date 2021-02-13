01931d7b31b55c25c726da4777c5d1c999d55966
divyach-prog <72274933+divyach-prog@users.noreply.github.com>
2021-02-14 00:15:13 +0530

    Add files via upload

---
+++ image.js
@@ -0,0 +1,6 @@
+function generate(){
+    var image = document.createElement('img');
+    var div = document.getElementById("cat");
+    image.src = "https://media.giphy.com/media/OmK8lulOMQ9XO/giphy.gif";
+    div.append(image);
+}
